856aa54bbe18d2102971b089c1bf64f1bb105952
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-04-04 13:10:14 -0300

    Se agrega la logica para segundos, minutos y horas

--- js/app.js
+++ js/app.js
@@ -1,35 +1,43 @@
-const cronometro = document.getElementById("cronometro");
+const cronometroEnPantalla = document.getElementById("cronometro");
 const btnPlay = document.getElementById("btnPlay");
 const btnPause = document.getElementById("btnPause");
 const btnReset = document.getElementById("btnReset");
-let segundosActuales = 0;
-let minutosActuales = 0;
+let segundosActuales = 50;
+let minutosActuales = 59;
 let horasActuales = 0;
 let bandera = 0;
 
 btnPlay.addEventListener("click", play);
 btnPause.addEventListener("click", pause);
 btnReset.addEventListener("click", reset);
-cronometro.addEventListener("onload", cronometroActualizado);
+cronometroEnPantalla.addEventListener("onload", cronometroActualizado);
 
-function cronometroActualizado() { cronometro.innerHTML = `${horasActuales}:${minutosActuales}:${segundosActuales}` }
+function cronometroActualizado() { cronometroEnPantalla.innerHTML = `${horasActuales}:${minutosActuales}:${segundosActuales}` }
 
 
 
 
 
 function play() {
-    let segundosI = setInterval(segundos, 1000);
+    let segundosI = setInterval(cronometro, 1000);
 }
 function pause() {
     bandera++;
     console.log(bandera);
 }
 function reset() {
-    cronometro.innerText = `Estoy en reset`
+    cronometroEnPantalla.innerText = `Estoy en reset`
 }
 
-function segundos() {
-    segundosActuales++;
+function cronometro() {
     cronometroActualizado();
+    segundosActuales++;
+    if(segundosActuales === 60){
+        minutosActuales++;
+        segundosActuales = 0;
+    }
+    if(minutosActuales === 60){
+        horasActuales++;
+        minutosActuales = 0;
+    }
 }
