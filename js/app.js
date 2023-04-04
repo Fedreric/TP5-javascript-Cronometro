e354531b1415160f96ce0e73ac3dfc9ae8dedd14
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-04-04 12:55:15 -0300

    se agrega la logica para los segundos

--- js/app.js
+++ js/app.js
@@ -2,11 +2,34 @@ const cronometro = document.getElementById("cronometro");
 const btnPlay = document.getElementById("btnPlay");
 const btnPause = document.getElementById("btnPause");
 const btnReset = document.getElementById("btnReset");
+let segundosActuales = 0;
+let minutosActuales = 0;
+let horasActuales = 0;
+let bandera = 0;
 
 btnPlay.addEventListener("click", play);
 btnPause.addEventListener("click", pause);
 btnReset.addEventListener("click", reset);
+cronometro.addEventListener("onload", cronometroActualizado);
 
-function play() {cronometro.innerText = `Estoy en play`}
-function pause() {cronometro.innerText = `Estoy en pausa`}
-function reset() {cronometro.innerText = `Estoy en reset`}
+function cronometroActualizado() { cronometro.innerHTML = `${horasActuales}:${minutosActuales}:${segundosActuales}` }
+
+
+
+
+
+function play() {
+    let segundosI = setInterval(segundos, 1000);
+}
+function pause() {
+    bandera++;
+    console.log(bandera);
+}
+function reset() {
+    cronometro.innerText = `Estoy en reset`
+}
+
+function segundos() {
+    segundosActuales++;
+    cronometroActualizado();
+}
